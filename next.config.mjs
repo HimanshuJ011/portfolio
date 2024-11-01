a40d3ed95501524f0979403a7559bcaae85b6461
HimanshuJ011 <joshihimanshu245@gmail.com>
2024-11-01 19:17:04 +0530

    added static 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/portfolio",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
-  
+  basePath: "/portfolio",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
+
+export default nextConfig;
