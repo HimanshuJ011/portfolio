69311c8b555bf1870909bd9b83d0e5ce0d50fe73
HimanshuJ011 <joshihimanshu245@gmail.com>
2024-11-01 19:19:51 +0530

    added static 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  basePath: "/portfolio",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
+    basePath: isProd ? '/portfolio' : '',
+    tagret: 'static',
+    distDir:"dist",
+    images:{
+        unoptimized:true
+    }
 };
 
 export default nextConfig;
